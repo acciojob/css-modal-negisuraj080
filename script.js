44e3d4e0b9af612ad55474578f83b26c4b8ae112
negisuraj080 <143009358+negisuraj080@users.noreply.github.com>
2023-11-24 13:28:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //your JS code here. If required.
-//your JS code here. If required.
 
 //function to open the modal
 function onOpenModal() {
@@ -7,6 +6,20 @@ function onOpenModal() {
      var btn = document.getElementById('openModal');
 	  modal.style.display = 'block';
 }
+
+// Function to close the modal
+function onCloseModal() {
+	var closeBtn = document.querySelector('.close-modal');
+	var modal = document.getElementById('myModal');
+	modal.style.display = 'none';
+}
+
+ window.addEventListener('click', function (event) {
+	 var modal = document.getElementById('myModal');
+	 if (event.target === modal) {
+          onCloseModal();
+        }
+});
 
 // Function to close the modal
 function onCloseModal() {
