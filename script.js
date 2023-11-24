4ea3c77575c1e37085575f09bfd58fa33e8869dc
negisuraj080 <143009358+negisuraj080@users.noreply.github.com>
2023-11-24 13:06:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+//your JS code here. If required.
+
+//function to open the modal
+function onOpenModal() {
+	 var modal = document.getElementById('myModal');
+     var btn = document.getElementById('openModal');
+	  modal.style.display = 'block';
+}
+
+// Function to close the modal
+function onCloseModal() {
+	var closeBtn = document.querySelector('.close-modal');
+	var modal = document.getElementById('myModal');
+	modal.style.display = 'none';
+}
